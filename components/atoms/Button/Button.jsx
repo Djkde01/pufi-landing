f12a06115aefd05bfffa77271ba2929e31374734
Djkde01 <sergio.estrella@utp.edu.co>
2022-10-15 01:16:21 -0500

    chore (Pre-commit hook)

--- components/atoms/Button/Button.jsx
+++ components/atoms/Button/Button.jsx
@@ -1,13 +1,15 @@
 import Link from "next/link";
 
-const Button = ({ text, link, type }) =>
-  type === "primary" ? (
+function Button({ text, link, type }) {
+  return type === "primary" ? (
     <Link href={link}>
-      <button className={`button button_${type}`}>{text}</button>
+      <button type="button" className={`button button_${type}`}>
+        {text}
+      </button>
     </Link>
   ) : (
     <Link href={link}>
-      <button className={`button button_${type}`}>
+      <button type="button" className={`button button_${type}`}>
         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24">
           <path d="M7.293 4.707 14.586 12l-7.293 7.293 1.414 1.414L17.414 12 8.707 3.293 7.293 4.707z" />
         </svg>
@@ -15,5 +17,6 @@ const Button = ({ text, link, type }) =>
       </button>
     </Link>
   );
+}
 
 export default Button;
